ce45c489e55100cd2c918357bbba3b56ff2dfc3c
seohee0112 <ghdtj7@naver.com>
2022-07-08 01:26:23 +0900

    [ feat ] 메인 페이지 레이아웃 구현

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,3 +1,34 @@
+import { MainBanner } from '../components/main';
+import styled from '@emotion/styled';
+import { ToyList } from '../components/common';
+
 export default function main() {
-  return <div>메인입니다</div>;
+  return (
+    <>
+      <MainBanner />
+      <StMainSection>
+        <StConceptArticle>
+          <StConceptTitle>이번주 인기 장난감</StConceptTitle>
+          <ToyList />
+        </StConceptArticle>
+      </StMainSection>
+    </>
+  );
 }
+
+const StMainSection = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+`;
+const StConceptArticle = styled.article`
+  padding: 7rem 37rem;
+`;
+const StConceptTitle = styled.div`
+  padding-bottom: 5rem;
+
+  font-weight: 500;
+  font-size: 2.6rem;
+  text-align: center;
+`;
