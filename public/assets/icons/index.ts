0291d1bb24b00005331f611275b787d555db6d84
seohee0112 <ghdtj7@naver.com>
2022-07-08 01:24:54 +0900

    [ feat ] 관심북마크 svg가져오기 구현

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,1 +1,4 @@
 export { default as IcNoriLogo } from './noriLogoIcon.svg';
+export { default as IcMainBanner } from './mainBannerIc.svg';
+export { default as IcToyMark } from './toyMark.svg';
+export { default as IcFillToyMark } from './fillToyMark.svg';
