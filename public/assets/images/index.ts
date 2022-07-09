c326d8bb4cc79a2cbff99d7f9228244b063cba56
seohee0112 <ghdtj7@naver.com>
2022-07-09 14:53:35 +0900

    [ style ] 로고 아이콘 설정

--- public/assets/images/index.ts
+++ public/assets/images/index.ts
@@ -1,2 +1,4 @@
 export { default as ImgNoriLogo } from './noriLogo.png';
-export { default as ImgKakaoLogin } from './kakao_login_large_narrow.png';
+export { default as ImgNaverLogo } from './naverLogo.png';
+export { default as ImgGoogleLogo } from './googleLogo.png';
+export { default as ImgKakaoLogo } from './kakaoLogo.png';
