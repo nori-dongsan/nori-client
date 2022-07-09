eaed762cad4e07ee921af6c1194c59b380ddf8bb
seohee0112 <ghdtj7@naver.com>
2022-07-09 14:53:57 +0900

    [ feat ] 네이버 로그인 버튼 클릭 및 토큰 가져오기만 구현

--- next.config.js
+++ next.config.js
@@ -13,5 +13,14 @@ const nextConfig = {
     return config;
   },
 };
-
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/oauth2.0/:path*', // url이 source에 해당될 경우
+        destination: 'https://nid.naver.com/oauth2.0/:path*', // destination으로 redirect
+      },
+    ];
+  },
+};
 module.exports = nextConfig;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,19 @@
-import React, { useEffect } from 'react';
-import { ImgKakaoLogin } from '../public/assets/images';
+import { useEffect, useRef, AnchorHTMLAttributes } from 'react';
+import { IcKakao } from '../public/assets/icons';
+import {
+  ImgKakaoLogo,
+  ImgGoogleLogo,
+  ImgNaverLogo,
+} from '../public/assets/images';
 import Image from 'next/image';
 import Router from 'next/router';
+import styled from '@emotion/styled';
 import { KakaoResParams } from '../types/api';
 
 export default function login() {
-  const handleLogin = () => {
+  const naverLoginRef = useRef<HTMLDivElement>(null);
+
+  const handleKakaoLogin = () => {
     const kakao = (window as any).Kakao;
     kakao.Auth.login({
       success: () => {
@@ -30,15 +38,86 @@ export default function login() {
     });
   };
 
+  const naverLogin = () => {
+    const naver = (window as any).naver;
+    const naverLogin = new naver.LoginWithNaverId({
+      clientId: process.env.NEXT_PUBLIC_NAVER_ID,
+      callbackUrl: 'http://localhost:3000/login', // Callback URL
+      isPopup: false /* 팝업을 통한 연동처리 여부, true 면 팝업 */,
+      loginButton: {
+        color: 'green',
+        type: 1,
+        height: 47,
+      } /* 로그인 버튼의 타입을 지정 */,
+    });
+    naverLogin.init();
+    const hash = Router.asPath.split('#')[1]; // 네이버 로그인을 통해 전달받은 hash 값
+    console.log(hash);
+    if (hash) {
+      const token = hash.split('=')[1].split('&')[0]; // token값 확인
+      naverLogin.getLoginStatus((status: any) => {
+        if (status) {
+          // 로그인 상태 값이 있을 경우
+          console.log(naverLogin.user); // 사용자 정보 조회
+          // /naver 페이지로 token값과 함께 전달 (서비스할 땐 token 전달을 하지 않고 상태 관리를 사용하는 것이 바람직할 것으로 보임)
+          Router.push({
+            pathname: '/naver',
+            query: {
+              token: token,
+            },
+          });
+        }
+      });
+    }
+  };
+  const handleNaverLogin = () => {
+    if (naverLoginRef.current) {
+      naverLoginRef.current.firstChild?.click();
+    }
+  };
   useEffect(() => {
     const kakao = (window as any).Kakao;
     if (!kakao.isInitialized())
       kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+
+    naverLogin();
   }, []);
 
   return (
-    <div>
-      <Image src={ImgKakaoLogin} onClick={handleLogin} />
-    </div>
+    <StLoginWrapper>
+      <Image
+        id="naverIdLogin"
+        src={ImgNaverLogo}
+        width={56}
+        height={56}
+        style={{ padding: '1.5rem' }}
+        onClick={handleNaverLogin}
+      />
+      <Image
+        src={ImgKakaoLogo}
+        width={56}
+        height={56}
+        style={{ padding: '1.5rem' }}
+        onClick={handleKakaoLogin}
+      />
+
+      <StNaverLoginDiv ref={naverLoginRef} id="naverIdLogin"></StNaverLoginDiv>
+      <Image
+        src={ImgGoogleLogo}
+        width={56}
+        height={56}
+        style={{ padding: '1.5rem' }}
+      />
+      <a></a>
+    </StLoginWrapper>
   );
 }
+const StNaverLoginDiv = styled.div`
+  display: none;
+`;
+
+const StLoginWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
