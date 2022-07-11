9a19a9b7422264118cee468fc9dd49da7b5ab42d
say-young516 <sychoi0516@naver.com>
2022-07-11 21:23:36 +0900

    [ feat ] 드롭다운 보이는 상태 설정

--- components/viewProduct/ProductFilter.tsx
+++ components/viewProduct/ProductFilter.tsx
@@ -50,37 +50,48 @@ const way = ['타고 노는', '만지고 노는'];
 const filterInfo = ['종류', '사용 연령', '가격', '특성', '스토어'];
 
 export default function ProductFilter() {
-  const [visibility, setvisibility] = useState<boolean[]>([]);
+  const [visibility, setVisibility] = useState<boolean[]>([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
   return (
-    // <StFilterWrapper>
-    //   <StFilterSection>
-    //     <h2>종류</h2>
-    //     <IcOpen />
-    //     {/* <FilterDropdown categoryInfo={kind} /> */}
-    //   </StFilterSection>
-    //   <StFilterSection>
-    //     <h2>사용 연령</h2>
-    //     <IcOpen />
-    //     {/* <FilterDropdown categoryInfo={useAge} /> */}
-    //   </StFilterSection>
-    //   <StFilterSection>
-    //     <h2>가격</h2>
-    //     <IcOpen />
-    //     {/* <FilterDropdown categoryInfo={cost} /> */}
-    //   </StFilterSection>
-    //   <StFilterSection>
-    //     <h2>특성</h2>
-    //     <IcOpen />
-    //     {/* <FilterDropdown categoryInfo={way} /> */}
-    //   </StFilterSection>
-    //   <StFilterSection>
-    //     <h2>스토어</h2>
-    //     <IcOpen />
-    //     {/* <FilterDropdown categoryInfo={store} /> */}
-    //   </StFilterSection>
-    // </StFilterWrapper>
-
-    <FilterDropdown categoryInfo={kind} />
+    <StFilterWrapper>
+      <StFilterSection>
+        <h2>종류</h2>
+        <IcOpen
+          onClick={() => {
+            setVisibility({
+              ...visibility,
+              [0]: !visibility[0],
+            });
+          }}
+        />
+        {visibility[0] && <FilterDropdown categoryInfo={kind} />}
+      </StFilterSection>
+      <StFilterSection>
+        <h2>사용 연령</h2>
+        <IcOpen />
+        {visibility[1] && <FilterDropdown categoryInfo={useAge} />}
+      </StFilterSection>
+      <StFilterSection>
+        <h2>가격</h2>
+        <IcOpen />
+        {visibility[2] && <FilterDropdown categoryInfo={cost} />}
+      </StFilterSection>
+      <StFilterSection>
+        <h2>특성</h2>
+        <IcOpen />
+        {visibility[3] && <FilterDropdown categoryInfo={way} />}
+      </StFilterSection>
+      <StFilterSection>
+        <h2>스토어</h2>
+        <IcOpen />
+        {visibility[4] && <FilterDropdown categoryInfo={store} />}
+      </StFilterSection>
+    </StFilterWrapper>
   );
 }
 
@@ -89,7 +100,11 @@ const StFilterWrapper = styled.div`
   height: 28rem;
   padding-left: 1.2rem;
 `;
-
+const StFilterTile = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 const StFilterSection = styled.section`
   display: flex;
   justify-content: space-between;
@@ -102,3 +117,28 @@ const StFilterSection = styled.section`
   border-bottom: 0.1rem #d9d9d9 solid;
   font: ${({ theme }) => theme.fonts.b4_15_semibold_146};
 `;
+
+// display `-객체의 노출여부/표현방식--`
+// ( justify-content / align-items)
+// ( flex-direction / flex-wrap / flex-flow ) → flex ~로 시작하는 것들
+// list-style
+// position `-위치/좌표--`
+// float
+// clear
+
+// width
+// height `-크기/여백--`
+// padding
+// margin
+
+// border
+// background `-윤곽/배경--`
+// color
+// font `-글자/정렬--`
+
+// text-decoration
+// text-align / vertical-align
+
+// white-space
+// other text
+// content `-내용--`
