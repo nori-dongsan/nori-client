e3e173067a10ddc8bf7a122530d3226cdeabacd1
seohee0112 <ghdtj7@naver.com>
2022-07-11 21:33:06 +0900

    [ docs ] 문서 재저장

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,9 +1,7 @@
 export { default as IcNoriHeaderLogo } from './noriHeaderIcon.svg';
-export {default as IcSearchIcon }  from './searchIcon.svg';
-export { default as IcNoriLogo } from './noriLogoIcon.svg';
+export { default as IcSearchIcon } from './searchIcon.svg';
 export { default as IcPlus } from './IcPlus.svg';
 export { default as IcNoriFooterLogo } from './noriFooterIcon.svg';
 export { default as IcMainBanner } from './mainBannerIc.svg';
 export { default as IcToyMark } from './toyMark.svg';
 export { default as IcFillToyMark } from './fillToyMark.svg';
-
