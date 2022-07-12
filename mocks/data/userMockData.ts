c787d27fdde70a98c5b13857e5e36121d840eb14
seohee0112 <ghdtj7@naver.com>
2022-07-12 17:08:36 +0900

    [ feat ] 유저, 장난감, 커뮤니티 mock 데이터 추가

---
+++ mocks/data/userMockData.ts
@@ -0,0 +1,10 @@
+import { UserData } from '../../types/user';
+export const userMockData: UserData[] = [
+  {
+    id: 'happhee',
+    sns_id: 'ghdtj7@naver.com',
+    provider: 'naver',
+    nickname: 'happhee',
+    email: 'ghdtj7@naver.com',
+  },
+];
