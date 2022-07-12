531bd81778b967ee3afc826c73fae89526b8bedd
Brokyeom <goindol40@gmail.com>
2022-07-12 17:56:36 +0900

    [  init  ] 댓글 컴포넌트 초기세팅

---
+++ pages/comment.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export default function Comment () {
+    return(
+        <StCommentWrapper>
+<h1>댓글</h1>
+        </StCommentWrapper>
+    )
+}
+
+const StCommentWrapper = styled.section`
+
+`;
