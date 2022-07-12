70322e7f36b3a00a5185172174b4f7cf559cb4f3
Brokyeom <goindol40@gmail.com>
2022-07-12 16:30:28 +0900

    [   feat  ] 카테고리 분류 구현

--- components/community/CommunityList.tsx
+++ components/community/CommunityList.tsx
@@ -10,25 +10,30 @@ export default function CommunityList() {
         <IcCommunitySearchIcon />
       </StSearchBar>
       <ContentCard
+        category="후기"
         title="그린키드 미끄럼틀 아이가 좋아하네요"
         content="군인 또는 군무원이 아닌 국민은 대한민국의 영역안에서는 중대한 군사상 기밀·초병·초소·유독음식물공급·포로·군용물에 관한 죄중 법률이 정한 경우와 비상계엄이 선포된 경우를 제외하고는 군사법원의 재판을 받지 아니한다. 선거에 관한 경비는 법률이 정하는 경우....경우와 비상계엄이 선포된 경우를 제외하고는 군사법원의 재판을 받지 아니한다. 선거에 관한 경비는 법률이 정하는 경우...."
         userNickname="예현맘"
         createdAt="2022.07.12"
-        replyCount="12"
+        replyCount={12}
+        img="https://www.littlebaby.co.kr:14019/shop/data/goods/1632018070797m0.jpg"
       />
       <ContentCard
+        category="정보공유"
         title="그린키드 미끄럼틀 아이가 좋아하네요"
         content="군인 또는 군무원이 아닌 국민은 대한민국의 영역안에서는 중대한 군사상 기밀·초병·초소·유독음식물공급·포로·군용물에 관한 죄중 법률이 정한 경우와 비상계엄이 선포된 경우를 제외하고는 군사법원의 재판을 받지 아니한다. 선거에 관한 경비는 법률이 정하는 경우....경우와 비상계엄이 선포된 경우를 제외하고는 군사법원의 재판을 받지 아니한다. 선거에 관한 경비는 법률이 정하는 경우...."
         userNickname="예현맘"
         createdAt="2022.07.12"
-        replyCount="127"
+        replyCount={127}
       />
       <ContentCard
+        category="질문"
         title="그린키드 미끄럼틀 아이가 좋아하네요"
         content="군인 또는 군무원이 아닌 국민은 대한민국의 영역안에서는 중대한 군사상 기밀·초병·초소·유독음식물공급·포로·군용물에 관한 죄중 법률이 정한 경우와 비상계엄이 선포된 경우를 제외하고는 군사법원의 재판을 받지 아니한다. 선거에 관한 경비는 법률이 정하는 경우....경우와 비상계엄이 선포된 경우를 제외하고는 군사법원의 재판을 받지 아니한다. 선거에 관한 경비는 법률이 정하는 경우...."
         userNickname="예현맘"
         createdAt="2022.07.12"
-        replyCount="3"
+        replyCount={3}
+        img="https://www.littlebaby.co.kr:14019/shop/data/goods/1632018070797m0.jpg"
       />
     </StCommunityListWrapper>
   );
--- components/community/ContentsCard.tsx
+++ components/community/ContentsCard.tsx
@@ -1,39 +1,52 @@
 import styled from '@emotion/styled';
+import { useState } from 'react';
 import { IcComment, IcHeart } from '../../public/assets/icons';
 
 interface ContentsInfo {
-    id?:number,
-    category?: string,
-    title: string,
-    content: string,
-    userNickname: string,
-    replyCount: string,
-    createdAt: string,
-    image?:string
+  category: string;
+  title: string;
+  content: string;
+  userNickname: string;
+  replyCount: number;
+  createdAt: string;
+  img?: string;
 }
 
 export default function ContentCard(props: ContentsInfo) {
-    const { id, category, title, content, userNickname, replyCount, createdAt, image } = props;
+  const { category, title, content, userNickname, replyCount, createdAt, img } =
+    props;
+
   return (
     <StContentsCardWrapper>
-      <h1>{title}</h1>
-      <p>{content}</p>
-      <StWriteInfo>
-        <span>{userNickname}</span>
-        <span>{createdAt}</span>
-      </StWriteInfo>
-      <StReplyInfo>
-      <IcComment />
-      <span>{replyCount}</span>
-      </StReplyInfo>
+      <StContentInfo>
+        {category === '후기' ? (
+          <StCategoryReview>{category}</StCategoryReview>
+        ) : category === '질문' ? (
+          <StCategoryQuestion>{category}</StCategoryQuestion>
+        ) : (
+          <StCategoryInfo>{category}</StCategoryInfo>
+        )}
+        <h1>{title}</h1>
+        <p>{content}</p>
+        <StWriteInfo>
+          <span>{userNickname}</span>
+          <span>{createdAt}</span>
+        </StWriteInfo>
+        <StReplyInfo>
+          <IcComment />
+          <span>{replyCount}</span>
+        </StReplyInfo>
+      </StContentInfo>
+      {img === undefined ? <></> : <StContentImg src={img} />};
     </StContentsCardWrapper>
   );
 }
 
 const StContentsCardWrapper = styled.div`
+  display: flex;
+
   width: 120rem;
   height: auto;
-
 
   margin-bottom: 9.6rem;
   padding-bottom: 4.5rem;
@@ -57,6 +70,45 @@ const StContentsCardWrapper = styled.div`
     margin-bottom: 2.7rem;
   }
 `;
+const StContentInfo = styled.section`
+  width: auto;
+
+  margin-right: 4.9rem;
+`;
+
+const StCategory = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 10.2rem;
+  height: 3.2rem;
+
+  margin-bottom: 2.6rem;
+
+  border-radius: 4.5rem;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 26px;
+
+  color: ${({ theme }) => theme.colors.white};
+`;
+const StCategoryReview = styled(StCategory)`
+  background-color: ${({ theme }) => theme.colors.mainGreen};
+`;
+const StCategoryQuestion = styled(StCategory)`
+  background-color: ${({ theme }) => theme.colors.black};
+`;
+const StCategoryInfo = styled(StCategory)`
+  background-color: ${({ theme }) => theme.colors.mainDarkgreen};
+`;
+
+const StContentImg = styled.img`
+  width: 26rem;
+  height: 25.5rem;
+`;
 const StWriteInfo = styled.div`
   margin-bottom: 1.2rem;
 
@@ -77,8 +129,8 @@ const StWriteInfo = styled.div`
   }
 `;
 const StReplyInfo = styled.div`
-display: flex;
-align-items: center;
+  display: flex;
+  align-items: center;
 
   span {
     margin-left: 1.057rem;
