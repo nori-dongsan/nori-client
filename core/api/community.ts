20facb401e74153947700d4d52b5b52b05f8a494
seohee0112 <ghdtj7@naver.com>
2022-07-13 14:43:06 +0900

    [ inite ] 유저, 장난감, 커뮤니티 api 초기 세팅

---
+++ core/api/community.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import useSWR from 'swr';
+import { baseInstance } from '../axios';
+
+export const useGetCollectionProduct = (key: string) => {
+  const { data } = useSWR(`/collection?sort=${key}`, baseInstance.get);
+  console.log(data);
+  return data;
+};
+export const getCollectionProduct = (key: string) => {
+  return baseInstance.get(`/collection?sort=${key}`);
+};
