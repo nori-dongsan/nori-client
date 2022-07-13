571c868351065b9457fac52dd47d13a96f80d85c
seohee0112 <ghdtj7@naver.com>
2022-07-13 14:42:43 +0900

    [ feat ] axios instance 생성

--- core/axios.ts
+++ core/axios.ts
@@ -2,9 +2,24 @@ import axios from 'axios';
 import LocalStorage from './localStorage';
 // const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-// const baseInstance = axios.create({
-//   baseURL: `${BASE_URL}`,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
+const baseInstance = axios.create({
+  baseURL: `/`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+// // client side base instance (default)
+// // 로컬스토리지 접근이 가능하고 token이 필요한 api 호출에서 사용
+baseInstance.interceptors.request.use((config) => {
+  const headers = {
+    ...config.headers,
+    Authorization: LocalStorage.getItem('nori'),
+  };
+
+  return { ...config, headers };
+});
+
+baseInstance.interceptors.response.use((response) => {
+  return response.data;
+});
+export { baseInstance };
