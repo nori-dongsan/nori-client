2b2f33d55c03d58b346873cd549c9c01986190ee
seohee0112 <ghdtj7@naver.com>
2022-07-13 14:46:50 +0900

    [ init ] msw 재설치

--- components/collectionProduct/PriceFilter.tsx
+++ components/collectionProduct/PriceFilter.tsx
@@ -8,7 +8,9 @@ import {
 export default function PriceFilter() {
   const data = useGetCollectionProduct('price-desc');
   console.log('낮은 가격순', data);
+
   const handlePriceDesc = () => {};
+
   const handlePriceAsc = async () => {
     const data = await getCollectionProduct('price-asc');
     console.log('높은 가격순 ', data);
