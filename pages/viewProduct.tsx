12756c460649c5817b3773cb1446cc48c5cadae8
seohee0112 <ghdtj7@naver.com>
2022-07-14 12:57:37 +0900

    [ docs ] 상품보기 padding 테스트 위한  코드 가져오기

--- pages/viewProduct.tsx
+++ pages/viewProduct.tsx
@@ -1,10 +1,40 @@
 import { ProductFilter, ViewProductBanner } from '../components/viewProduct';
+import styled from '@emotion/styled';
+import { ToyList } from '../components/common';
 
 export default function viewProduct() {
   return (
-    <div>
+    <StWrapper>
       <ViewProductBanner />
-      <ProductFilter />
-    </div>
+
+      <StViewProductWrapper>
+        <StFilterWrapper>
+          <ProductFilter />
+        </StFilterWrapper>
+        <StToyListWrapper>
+          {/* <ToyList landingCategory="viewProduct" length={4} /> */}
+        </StToyListWrapper>
+      </StViewProductWrapper>
+    </StWrapper>
   );
 }
+
+const StWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 0rem 37.2rem;
+`;
+const StViewProductWrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+`;
+const StFilterWrapper = styled.nav`
+  display: flex;
+  flex: 0.5;
+`;
+const StToyListWrapper = styled.article`
+  display: flex;
+  flex: 2.5;
+`;
