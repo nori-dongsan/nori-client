d8eec9afe541786b917015dadc7c52b080255eec
seohee0112 <ghdtj7@naver.com>
2022-07-14 17:03:28 +0900

    [ feat ] 로그인 페이지 레이아웃 구현

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,34 +6,52 @@ import {
 import Image from 'next/image';
 import styled from '@emotion/styled';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { IcLoginLine } from '../public/assets/icons';
+import Link from 'next/link';
 
 export default function login() {
   const { data, status } = useSession();
   console.log(data?.user);
   return (
     <StLoginWrapper>
-      <Image
-        src={ImgNaverLogo}
-        width={56}
-        height={56}
-        style={{ padding: '1.5rem' }}
-        onClick={() => signIn('naver')}
-      />
-      <Image
-        src={ImgKakaoLogo}
-        width={56}
-        height={56}
-        style={{ padding: '1.5rem' }}
-        onClick={() => signIn('kakao')}
-      />
+      <StLoginTitle>로그인</StLoginTitle>
+      <IcLoginLine />
+      <StContentWrapper>
+        <StContent>다양한 장난감 정보를 한눈에!</StContent>
+        <StContent>우리 아이 장난감 대여 비교 플랫폼</StContent>
+      </StContentWrapper>
+      <StTextWrapper>
+        <StContentSpan>SNS 계정으로 간편 로그인 /</StContentSpan>
+        <StLoginLink href="/signup">
+          <StSignUpA>회원가입</StSignUpA>
+        </StLoginLink>
+      </StTextWrapper>
+      <StSocialLoginWrapper>
+        <Image
+          src={ImgNaverLogo}
+          width={56}
+          height={56}
+          style={{ padding: '1.5rem' }}
+          onClick={() => signIn('naver')}
+        />
+        <Image
+          src={ImgKakaoLogo}
+          width={56}
+          height={56}
+          style={{ padding: '1.5rem' }}
+          onClick={() => signIn('kakao')}
+        />
 
-      <Image
-        src={ImgGoogleLogo}
-        width={56}
-        height={56}
-        style={{ padding: '1.5rem' }}
-        onClick={() => signIn('google')}
-      />
+        <Image
+          src={ImgGoogleLogo}
+          width={56}
+          height={56}
+          style={{ padding: '1.5rem' }}
+          onClick={() => signIn('google')}
+        />
+      </StSocialLoginWrapper>
+
+      <IcLoginLine />
     </StLoginWrapper>
   );
 }
@@ -41,4 +59,48 @@ const StLoginWrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+
+  padding-top: 4.6rem;
+  padding-bottom: 18.8rem;
+`;
+const StLoginTitle = styled.h1`
+  padding-bottom: 6.1rem;
+
+  ${({ theme }) => theme.fonts.t1_28_medium_150}
+`;
+const StSocialLoginWrapper = styled.article`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 26rem;
+  padding-bottom: 7.4rem;
+`;
+const StContentWrapper = styled.div`
+  padding-top: 10.2rem;
+`;
+const StContent = styled.div`
+  color: #707070;
+  ${({ theme }) => theme.fonts.b2_18_regular_130};
+`;
+const StContentSpan = styled.span`
+  color: #707070;
+  ${({ theme }) => theme.fonts.b2_18_regular_130};
+`;
+const StTextWrapper = styled.article`
+  display: flex;
+
+  padding-top: 9.3rem;
+  padding-bottom: 2.1rem;
+`;
+const StLoginLink = styled(Link)`
+  display: inline;
+`;
+const StSignUpA = styled.a`
+  display: inline;
+
+  color: #707070;
+  ${({ theme }) => theme.fonts.b2_18_regular_130};
+  cursor: pointer;
 `;
