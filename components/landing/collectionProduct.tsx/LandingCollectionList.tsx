23de766c5e5841cbd258d48a6ee95c124d4f0a74
seohee0112 <ghdtj7@naver.com>
2022-07-16 19:05:15 +0900

    [ feat ] 컬렉션 모아보기 제목 랜딩 컴포넌트 구현

---
+++ components/landing/collectionProduct.tsx/LandingCollectionList.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import { LandingToyPreview } from '.';
+export default function LandingCollectionList() {
+  const toyList = new Array(4).fill(0);
+  return (
+    <StLandingToyListWrapper>
+      {toyList.map((_, idx) => (
+        <StToyPriewWrapper key={idx}>
+          <LandingToyPreview />
+        </StToyPriewWrapper>
+      ))}
+    </StLandingToyListWrapper>
+  );
+}
+
+const StLandingToyListWrapper = styled.section`
+  display: flex;
+`;
+const StToyPriewWrapper = styled.section`
+  margin: 0 1.25rem 6.5rem;
+`;
