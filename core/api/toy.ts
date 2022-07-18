9b721d2441af32ffcb5757a70291d57a3e2a3688
seohee0112 <ghdtj7@naver.com>
2022-07-19 03:41:42 +0900

    [ docs ] 주석

--- core/api/toy.ts
+++ core/api/toy.ts
@@ -1,21 +1,26 @@
-
 import axios from 'axios';
 
 import useSWR from 'swr';
 import { baseInstance } from '../axios';
 
 export const useGetCollectionProduct = (key: string) => {
-  const { data, error } = useSWR(`/collection?sort=${key}`, baseInstance.get, {
-    errorRetryCount: 3,
-  });
+  const { data, error } = useSWR(
+    // `https://jsonplaceholder.typicode.com/todos`,
+    `/collection?sort=${key}`,
+    baseInstance.get,
+    {
+      errorRetryCount: 3,
+    },
+  );
 
+  console.log(data);
   return {
     productList: data,
     isLoading: !error && !data,
     isError: error,
   };
-
 };
 export const getCollectionProduct = (key: string) => {
+  // return baseInstance.get(`https://jsonplaceholder.typicode.com/todos`);
   return baseInstance.get(`/collection?sort=${key}`);
 };
