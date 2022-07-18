88fffdd5d328932af0b586a42913feaaeee0919d
say-young516 <sychoi0516@naver.com>
2022-07-19 04:29:14 +0900

    [ style ] 태그 섹션 컴포넌트화

---
+++ components/viewProduct/TagSection.tsx
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+import { IcUndoBtn } from '../../public/assets/icons';
+import FilterTag from './FilterTag';
+
+export default function TagSection() {
+  return (
+    <StTagSection>
+      <StTagWrapper>
+        <FilterTag tagName={'노리가'} />
+        <FilterTag tagName={'노리가'} />
+        <FilterTag tagName={'노리가'} />
+        <FilterTag tagName={'노리가'} />
+        <FilterTag tagName={'노리가'} />
+        <FilterTag tagName={'노리가'} />
+        <FilterTag tagName={'노리가'} />
+        <FilterTag tagName={'노리가'} />
+        <FilterTag tagName={'노리가'} />
+      </StTagWrapper>
+      <StUndoAllTagBtn>
+        <h2>모두 해제</h2>
+        <IcUndoBtn />
+      </StUndoAllTagBtn>
+    </StTagSection>
+  );
+}
+
+const StTagWrapper = styled.article`
+  display: flex;
+  justify-content: flex-start;
+  flex-flow: row wrap;
+
+  width: 77.6rem;
+  height: fit-content;
+`;
+const StTagSection = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  padding: 1.5rem 0;
+  margin-left: 2.4rem;
+  border-bottom: 0.1rem solid ${({ theme }) => theme.colors.gray002};
+`;
+const StUndoAllTagBtn = styled.button`
+  display: flex;
+  align-items: center;
+
+  padding: 0 0 0 1rem;
+  margin-top: 0.5rem;
+
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 0.6rem;
+  border: 0.1rem solid ${({ theme }) => theme.colors.gray004};
+  color: ${({ theme }) => theme.colors.gray007};
+  ${({ theme }) => theme.fonts.b5_14_medium_140};
+`;
--- components/viewProduct/index.ts
+++ components/viewProduct/index.ts
@@ -4,3 +4,4 @@ export { default as FilterDropdown } from './FilterDropdown';
 export { default as ToyList } from './ToyList';
 export { default as ToyPreview } from './ToyPreview';
 export { default as FilterTag } from './FilterTag';
+export { default as TagSection } from './TagSection';
