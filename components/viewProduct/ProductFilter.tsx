c29326c40c81da0efaf508518805758f5a55a090
seohee0112 <ghdtj7@naver.com>
2022-07-20 23:15:13 +0900

    [ fix ] atom 적용

--- components/viewProduct/ProductFilter.tsx
+++ components/viewProduct/ProductFilter.tsx
@@ -3,59 +3,15 @@ import { LoaderValue } from 'next/dist/shared/lib/image-config';
 import React, { EventHandler, useState } from 'react';
 import { IcClose, IcOpen } from '../../public/assets/icons';
 import FilterDropdown from './FilterDropdown';
-
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { filterListState } from '../../core/atom';
 interface ProductFilterIcon {
   title: string;
   value: boolean;
 }
 export default function ProductFilter() {
-  const filterList = {
-    스토어: [
-      '국민장난감',
-      '그린키드',
-      '러브로',
-      '리틀베이비',
-      '빌리바바',
-      '어텐션홈이벤트',
-      '장난감점빵',
-      '젤리바운스',
-      '해피장난감',
-    ],
-    '사용 연령': [
-      '0~5개월',
-      '6~11개월',
-      '12~17개월',
-      '18~23개월',
-      '24~35개월',
-      '36~47개월',
-      '48~60개월',
-      '기타',
-    ],
-    가격: ['1만원 미만', '1-3만원', '3-5만원', '5-8만원', '8만원이상'],
-    특성: ['타고 노는', '만지고 노는', '기타'],
-    '장난감 종류': [
-      '아기체육관',
-      '모빌',
-      '바운서',
-      '쏘서',
-      '점퍼루',
-      '위고',
-      '보행기',
-      '걸음마 보조기',
-      '러닝홈',
-      '러닝테이블',
-      '기타 학습완구',
-      '미끄럼틀',
-      '에어바운스',
-      '트램펄린',
-      '어린이 자동차',
-      '흔들말',
-      '그네',
-      '소꿉놀이',
-      '역할놀이',
-      '기타',
-    ],
-  };
+  const { filterList } = useRecoilValue(filterListState);
+
   const filterListData = Object.values(filterList);
   const filterListKeys = Object.keys(filterList);
   const [visibility, setVisibility] = useState<boolean[]>([
@@ -72,6 +28,8 @@ export default function ProductFilter() {
     new Set<number>(),
     new Set<number>(),
   ]);
+  console.log(checkedItems);
+
   const handleDropdown = (idx: number) => {
     setVisibility({
       ...visibility,
@@ -124,6 +82,7 @@ export default function ProductFilter() {
               isExcept={idx == 3 ? true : false}
               isDrop={visibility[idx]}
               checkedItem={checkedItems[idx]}
+              categoryKey={title}
               handleCheckedItems={handleCheckedItems}
             />
           )}
