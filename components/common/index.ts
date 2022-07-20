b323d76cef464cdc7b095f02faa72cd283814bad
seohee0112 <ghdtj7@naver.com>
2022-07-21 00:05:18 +0900

    [ feat ] common 에 페이지네이션 구현

--- components/common/index.ts
+++ components/common/index.ts
@@ -3,3 +3,4 @@ export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as WriteHeader } from './WriteHeader';
 export { default as PriceFilter } from './PriceFilter';
+export { default as PageNavigation } from './PageNavigation';
